806b9671a308aac65ba7c8a9179d78b0a856b016
Yury Misiyevich <vinchesterok@tut.by>
2020-01-29 22:55:20 +0300

    feat: add menu animation and right selection

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 window.onload = function(){
-    let container = document.getElementsByClassName('container')[0];
-    let temp = document.getElementById('element1');
-    container.childNodes.forEach(label=>{
-        label.childNodes
-        if(label.querySelector('input').checked === true){
-            label.style.borderBottom = '5px solid black';
-        }
-    })
-}
+  let container = document.getElementsByClassName('container')[0];
+  let temp = document.getElementById('element1');
+  let inputs = document.getElementsByName('menu');
+  inputs.forEach(input=>{
+    console.log(input.labels[0]);
+    input.onchange = function(){
+      if(input.checked === true){
+        document.getElementById('abc').style.color="red";
+      }
+    }
+  })
+};
 
