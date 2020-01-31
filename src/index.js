deffbef0adb13cb56070ef00b7a77a8d3e5ae0c1
Yury Misiyevich <misiyevich@gmail.com>
2020-01-31 12:12:23 +0300

    feat: add animation for hamburger menu, add some style

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 window.onload = function(){
-  let lastCheckedMenuItem=null;
   let container = document.getElementsByClassName('container')[0];
   let hider = document.getElementById('hider');
   let content = document.getElementById('content');
 
+  //add darken wrapper
   container.childNodes.forEach(node=>{
     node.onmouseover = function(){
       if(node.children.length>2){
@@ -15,15 +15,38 @@ window.onload = function(){
     }
   });
 
-  //click handler for hamburger
-  document.getElementsByClassName('mobile-header')[0].onclick = function(){
+  //click handler for hamburger and add some animation with class-helper 'fade'
+  document.getElementsByClassName('ham-icon')[0].onclick = function(){
     if(container.style.display==='grid'){
-      container.style.display = 'none'
+      container.classList.add('fade');
+      container.style.display = 'none';
+      setTimeout(()=>{
+        container.classList.remove('fade');
+      },2000);
+      this.classList.add('fade');
+      this.innerHTML='&#9776';
+      setTimeout(()=>{
+        this.classList.remove('fade');
+      },2000);
+
     }
     else{
+      container.classList.add('fade');
       container.style.display = 'grid';
+      setTimeout(()=>{
+        container.classList.remove('fade');
+      },2000);
+
+
+      this.classList.add('fade');
+      this.innerHTML='&#10006';
+      setTimeout(()=>{
+        this.classList.remove('fade');
+      },2000);
+
     }
 
   }
+  //add icon for menu items with sublists
 };
 
