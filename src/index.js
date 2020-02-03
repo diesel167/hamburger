5b006b9d504d5a70efad2920b44b31054519489d
Yury Misiyevich <misiyevich@gmail.com>
2020-02-03 15:54:44 +0300

    add logic for second floor submenus, finish

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 window.onload = function(){
   let container = document.getElementsByClassName('container')[0];
   let hider = document.getElementById('hider');
-  let content = document.getElementById('content');
-  let inputs = document.getElementsByName('menu');
-  let lastOpened = null;
 
   //add darken wrapper
-  container.childNodes.forEach(node=>{
+
+  [...container.children].forEach(node=>{
     node.onmouseover = function(){
       if(node.children.length>2){
         hider.style.display='block';
       }
     };
     node.onmouseleave = function(){
-      hider.style.display='none';
+      if(document.getElementsByClassName('ham-icon')[0].style==='none'){
+        hider.style.display='none';
+      }
     }
+
   });
 
   //click handler for hamburger and add some animation with class-helper 'fade'
   document.getElementsByClassName('ham-icon')[0].onclick = function(){
-    if(container.style.display==='grid'){
+     if(container.style.display==='grid'){
+      hider.style.display='none';
       container.classList.add('fade');
       container.style.display = 'none';
       setTimeout(()=>{
@@ -33,6 +35,7 @@ window.onload = function(){
 
     }
     else{
+      hider.style.display='block';
       container.classList.add('fade');
       container.style.display = 'grid';
       setTimeout(()=>{
@@ -51,59 +54,82 @@ window.onload = function(){
   }
 
   //add icon for menu items with sublists
-  function expand(item){
+  function expand(item,nch){
+    let lastOpened = null;
+
     item = [...item];
     item.map((node,i)=>{
-      if(node.children.length>2){
+      if(node.children.length>nch){
         let expander = document.createElement('i');
         expander.className = 'expander';
         expander.id = `st${i}`;
-        expander.onclick = function(){
-          console.log(this.parentNode!==lastOpened);
-          if(this.parentNode!==lastOpened){
-            if(lastOpened){
-              lastOpened.querySelector('.submenu').style.display = 'none';
-              lastOpened.querySelector('i').style.transform = 'rotate(45deg)';
-              this.parentNode.querySelector('.submenu').style.display = 'block';
-              expander.style.transform = 'rotate(225deg)';
+        //if main submenus
+        if(nch===2){
+          expander.onclick = function(){
+            if(this.parentNode!==lastOpened){
+              if(lastOpened){
+                lastOpened.querySelector('.submenu').style.display = 'none';
+                lastOpened.querySelector('i').style.transform = 'rotate(45deg)';
+                this.parentNode.querySelector('.submenu').style.display = 'block';
+                expander.style.transform = 'rotate(225deg)';
+              }
+              else{
+                this.parentNode.querySelector('.submenu').style.display = 'block';
+                expander.style.transform = 'rotate(225deg)';
+              }
+              lastOpened = this.parentNode;
             }
             else{
-              this.parentNode.querySelector('.submenu').style.display = 'block';
-              expander.style.transform = 'rotate(225deg)';
-            }
-            lastOpened = this.parentNode;
-          }
-          else{
-            if(this.parentNode.querySelector('.submenu').style.display === 'none'){
-              this.parentNode.querySelector('.submenu').style.display = 'block';
-              expander.style.transform = 'rotate(225deg)';
-            }
-            else{
-              this.parentNode.querySelector('.submenu').style.display = 'none';
-              expander.style.transform = 'rotate(45deg)';
+              if(this.parentNode.querySelector('.submenu').style.display === 'none'){
+                this.parentNode.querySelector('.submenu').style.display = 'block';
+                expander.style.transform = 'rotate(225deg)';
+              }
+              else{
+                this.parentNode.querySelector('.submenu').style.display = 'none';
+                expander.style.transform = 'rotate(45deg)';
+              }
+
             }
 
-          }
-        };
+          };
+        }
+        //if second floor submenus
+        else{
+          expander.onclick = function(){
+            if(this.parentNode!==lastOpened){
+              if(lastOpened){
+                lastOpened.querySelector('ul').style.display = 'none';
+                lastOpened.querySelector('i').style.transform = 'rotate(45deg)';
+                this.parentNode.querySelector('ul').style.display = 'block';
+                expander.style.transform = 'rotate(225deg)';
+              }
+              else{
+                this.parentNode.querySelector('ul').style.display = 'block';
+                expander.style.transform = 'rotate(225deg)';
+              }
+              lastOpened = this.parentNode;
+            }
+            else{
+              if(this.parentNode.querySelector('ul').style.display === 'none'){
+                this.parentNode.querySelector('ul').style.display = 'block';
+                expander.style.transform = 'rotate(225deg)';
+              }
+              else{
+                this.parentNode.querySelector('ul').style.display = 'none';
+                expander.style.transform = 'rotate(45deg)';
+              }
+            }
+          };
+        }
         node.appendChild(expander);
-        //expander = document.createElement('i');
-        //expander.className = 'expander';
-        //node.children[2].appendChild(expander);
       }
     })
   }
 
-  /*inputs.forEach(input=>{
-    input.onchange = function(){
-      if(input.checked === true){
-        input.parentNode.color = "white";
-      }
-    }
-  });*/
-
-
-
-
-  expand(document.getElementsByClassName('nav-item'));
+  //create expander in our DOM
+  expand(document.getElementsByClassName('nav-item'),2);
+  [...document.getElementsByClassName('sub')].map(node=>node.children).map(part=>{
+    expand(part,1);
+  });
 };
 
